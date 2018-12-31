56e51caf07c942d30b72c40e8c3ca7ac2a94722b
KowsalyaPK <kowsalyapk@gmai.com>
2018-12-31 21:09:19 +0530

    Initial set up for UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+
 import './app/App.css';
+import '../style/index.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="/assets/logo.svg" className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid item xs={12} className="full-height">
+        <Grid container direction="row" alignItems="stretch" className="full-height">
+          <Grid item xs={12}>
+            Stock Management App
+          </Grid>
+         </Grid>
+      </Grid>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
