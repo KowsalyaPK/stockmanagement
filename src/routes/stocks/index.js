2dcd250ab4b1bcf7c7dc5fd8ddd736b2948eeda8
KowsalyaPK <kowsalyapk@gmai.com>
2019-01-02 12:03:58 +0530

    Add file upload input

--- src/routes/stocks/index.js
+++ src/routes/stocks/index.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 
 
 export default class StockComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            uploadedFile: ''
+        };
+    }
+    
+    handleFileInput = (event) => {
+        console.log(event.target.files);
+    }
+
     render() {
         return (
             <div>
                 Stocks
+                <form>
+                    <div className="form-group relative">
+                        <input type="file" onChange={this.handleFileInput} className="file-input" name="stock-file" id="stock-file" accept=".csv"
+                            required />
+                        <label htmlFor="stock-file">Upload file</label>
+                    </div>
+                </form>
             </div>
         )
     }
